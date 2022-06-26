7e0bffb7e5292a3bb5d6adcbebcb9103f73ccc59
Enrique M. Talavera <kiquetal@gmail.com>
2022-06-25 23:24:48 -0400

    first logic

--- Connect Four/task/src/connectfour/Main.kt
+++ Connect Four/task/src/connectfour/Main.kt
@@ -1,5 +1,13 @@
 package connectfour
 
 fun main() {
-    println("Hello World!")
+    println("Connect Four")
+    println("First player's name:")
+    val player1 = readLine()!!
+    println("Second player's name:")
+    val player2 = readLine()!!
+    println("Set the board dimensions (Rows x Columns)")
+    println("Press Enter for default (6 X 7)")
+    val input = readLine()!!
+
 }
